db6618e7b72678ea1ba0d66e8e66f6521dc67774
elon.kwon <elon.kwon@kakaocorp.com>
2018-11-19 13:44:38 +0900

    set sleep

--- src/main/java/com/elon/apiserver/controller/BookStoreCotroller.java
+++ src/main/java/com/elon/apiserver/controller/BookStoreCotroller.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookStoreCotroller {
 
     @RequestMapping(value = "/recommended")
-    public String readingList(){
+    public String readingList() throws InterruptedException {
+        Thread.sleep(300);
         return "Spring in Action (Manning), Cloud Native Java (O'Reilly), Learning Spring Boot (Packt)";
     }
 }
